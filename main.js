2ab48ecbabd4cbfa4bf098bd704dde79b7d75749
BarrocasVitoria <vitoriabarrocas2018@gmail.com>
2021-06-22 01:56:54 -0300

    Add js no site para o menu

---
+++ main.js
@@ -0,0 +1,18 @@
+/* Abre e fecha o menu quando clicar no hamburguer e no x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show') //aparecer e sumir o menu no "container show"
+  })
+}
+
+/* Quando clicar em um item no menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
